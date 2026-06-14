b5ee21fe1e8f02cfad44571e750f1eeb80f9bc87
Bhavesh189 <deva.pndit86@gmail.com>
2026-06-14 12:44:42 +0530

    CORS error into the frontend solved

--- index.js
+++ index.js
@@ -19,10 +19,12 @@ const app = express();
 
 connectDB();
 
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
-}));
+const corsOptions = {
+  origin: ["https://study-top.vercel.app", "http://localhost:5173"],
+  credentials: true
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
